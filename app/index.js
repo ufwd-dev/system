a711484da9a9568b18320bc441024b4c634d0ca3
Molly Shan <shenxiaohan@or-change.cn>
2018-03-28 19:02:28 +0800

    [FIXED] Restructure and transfer

--- app/index.js
+++ app/index.js
@@ -1,60 +1,38 @@
 'use strict';
 import app from 'app';
+import Home from 'app/component/Home.vue';
 
 import en_US from './i18n/en_US.yaml';
 import zh_CN from './i18n/zh_CN.yaml';
 
-app.menu.addGroup('ufwd.system', [
-	{
-		label: 'menu.account',
-		path: '/ufwd/account'
-	},
-	{
-		label: 'menu.channel',
-		path: '/ufwd/channel'
-	},
-	{
-		label: 'menu.activity',
-		path: '/ufwd/activity'
-	},
-	{
-		label: 'menu.article',
-		path: '/ufwd/article'
-	},
-	{
-		label: 'menu.vote',
-		path: '/ufwd/vote'
-	},
-]);
+app.i18n.mergeLocaleMessage('en', en_US);
+app.i18n.mergeLocaleMessage('zh', zh_CN);
 
-import Home from 'app/component/Home.vue';
+app.i18n.locale = 'en';
 
 import Account from  './component/account/Account.vue';
 import Info from  './component/account/Info.vue';
 import Manage from  './component/account/Manage.vue';
 import Category from  './component/account/Category.vue';
 
-import Channel from  './component/channel/Channel.vue';
-import New from  './component/channel/New.vue';
-
-import Activity from  './component/activity/Activity.vue';
-import ActivityDetail from  './component/activity/Detail.vue';
-
-import Article from  './component/article/Article.vue';
-
-import Vote from  './component/vote/Index.vue';
-import VoteDetail from  './component/vote/Vote.vue';
+app.menu.addGroup('ufwd.account', [
+	{
+		label: 'account.account',
+		path: '/ufwd/account/all'
+	},
+]);
 
 app.router.addRoutes([
 	{
-		path: '/ufwd',
+		path: '/ufwd/account',
 		component: Home,
 		meta: {
 			requireAccount: true
 		},
 		children: [
 			{
-				path: 'account',
+				path: 'all',
+				alias: '/',
 				component: Account
 			},
 			{
@@ -62,50 +40,84 @@ app.router.addRoutes([
 				component: Info
 			},
 			{
-				path: 'account/manage',
+				path: 'manage',
 				component: Manage
 			},
 			{
-				path: 'account/manage/:id/category',
+				path: 'manage/:id/category',
 				component: Category
-			},
-
-			{
-				path: 'channel',
-				component: Channel
-			},
-			{
-				path: 'channel/new',
-				component: New
-			},
-
-			{
-				path: 'activity',
-				component: Activity
-			},
-			{
-				path: 'activity/:id/detail',
-				component: ActivityDetail
-			},
-
-			{
-				path: 'article',
-				component: Article
-			},
-
-			{
-				path: 'vote',
-				component: Vote
-			},
-			{
-				path: 'vote/:id/detail',
-				component: VoteDetail
 			},
 		]
 	}
 ]);
 
-app.i18n.mergeLocaleMessage('en', en_US);
-app.i18n.mergeLocaleMessage('zh', zh_CN);
+import Channel from  './component/channel/Channel.vue';
+import New from  './component/channel/New.vue';
 
-app.i18n.locale = 'en';
+app.menu.addGroup('ufwd.channel', [
+	{
+		label: 'channel.channel',
+		path: '/ufwd/channel'
+	},
+	{
+		label: 'channel.new',
+		path: '/ufwd/channel/new'
+	}
+]);
+
+app.router.addRoutes([
+	{
+		path: '/ufwd/channel',
+		component: Home,
+		meta: {
+			requireAccount: true
+		},
+		children: [
+			{
+				path: '/',
+				component: Channel
+			},
+			{
+				path: 'new',
+				component: New
+			}
+		]
+	}
+]);
+
+app.menu.addGroup('ufwd.administrator', [
+	{
+		label: 'item.administrator',
+		path: '/ufwd/administrator'
+	}
+]);
+app.menu.addGroup('ufwd.writer', [
+	{
+		label: 'item.writer',
+		path: '/ufwd/writer'
+	}
+]);
+app.menu.addGroup('ufwd.notification', [
+	{
+		label: 'item.notification',
+		path: '/ufwd/notification'
+	}
+]);
+app.menu.addGroup('ufwd.subscribe', [
+	{
+		label: 'item.subscribe',
+		path: '/ufwd/subscribe'
+	}
+]);
+app.menu.addGroup('ufwd.advice', [
+	{
+		label: 'item.advice',
+		path: '/ufwd/advice'
+	}
+]);
+app.menu.addGroup('ufwd.group', [
+	{
+		label: 'item.group',
+		path: '/ufwd/group'
+	}
+]);
